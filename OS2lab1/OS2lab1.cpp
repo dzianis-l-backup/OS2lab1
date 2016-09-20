669ffbc3557f976e25c96319aa14eb0273ed41ab
DziLean <gestaltldi@yandex.ru>
2016-09-20 05:35:39 +0300

    line painting was added

--- OS2lab1/OS2lab1.cpp
+++ OS2lab1/OS2lab1.cpp
@@ -4,6 +4,10 @@
 #include "stdafx.h"
 #include "OS2lab1.h"
 #include <vector>
+#include <gdiplus.h>
+
+#pragma comment (lib,"Gdiplus.lib")
+using namespace Gdiplus;
 using namespace std;
 
 #define MAX_LOADSTRING 100
@@ -84,6 +88,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	wcex.lpszClassName	= szWindowClass;
 	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
+	
 	return RegisterClassEx(&wcex);
 }
 
@@ -117,6 +122,17 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
+/////////////////////////////////////////////
+//methods here
+VOID OnPaint(HDC hdc)//line drawn
+{
+	Graphics graphics(hdc);
+	Pen      pen(Color(255, 0, 0, 255));
+	graphics.DrawLine(&pen, 0, 0, 200, 100);
+}
+
+
+
 //
 //  ‘”Ќ ÷»я: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -132,15 +148,120 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
 	HDC hdc;
-
+	////////////////////////////////////////////
 	//variable scope
 	POINT t; 
 	static std::vector<POINT> pos; 
 	static bool status; 
 	PAINTSTRUCT pt;
+	////////////////////////////////////////////
+
+	RECT rcClient;                 // client area rectangle 
+	POINT ptClientUL;              // client upper left corner 
+	POINT ptClientLR;              // client lower right corner 
+	static POINTS ptsBegin;        // beginning point 
+	static POINTS ptsEnd;          // new endpoint 
+	static POINTS ptsPrevEnd;      // previous endpoint 
+	static BOOL fPrevLine = FALSE; // previous line flag 
 
 	switch (message)
 	{
+	case WM_LBUTTONDOWN:
+
+		// Capture mouse input. 
+
+		SetCapture(hWnd);
+
+		// Retrieve the screen coordinates of the client area, 
+		// and convert them into client coordinates. 
+
+		GetClientRect(hWnd, &rcClient);
+		ptClientUL.x = rcClient.left;
+		ptClientUL.y = rcClient.top;
+
+		// Add one to the right and bottom sides, because the 
+		// coordinates retrieved by GetClientRect do not 
+		// include the far left and lowermost pixels. 
+
+		ptClientLR.x = rcClient.right + 1;
+		ptClientLR.y = rcClient.bottom + 1;
+		ClientToScreen(hWnd, &ptClientUL);
+		ClientToScreen(hWnd, &ptClientLR);
+
+		// Copy the client coordinates of the client area 
+		// to the rcClient structure. Confine the mouse cursor 
+		// to the client area by passing the rcClient structure 
+		// to the ClipCursor function. 
+
+		SetRect(&rcClient, ptClientUL.x, ptClientUL.y,
+			ptClientLR.x, ptClientLR.y);
+		ClipCursor(&rcClient);//cursor constrains
+
+		// Convert the cursor coordinates into a POINTS 
+		// structure, which defines the beginning point of the 
+		// line drawn during a WM_MOUSEMOVE message. 
+
+		ptsBegin = MAKEPOINTS(lParam);
+		return 0;
+
+	case WM_MOUSEMOVE:
+
+		// When moving the mouse, the user must hold down 
+		// the left mouse button to draw lines. 
+
+		if (wParam & MK_LBUTTON)
+		{
+
+			// Retrieve a device context (DC) for the client area. 
+
+			hdc = GetDC(hWnd);
+
+			// The following function ensures that pixels of 
+			// the previously drawn line are set to white and 
+			// those of the new line are set to black. 
+
+			SetROP2(hdc, R2_NOTXORPEN);
+
+			// If a line was drawn during an earlier WM_MOUSEMOVE 
+			// message, draw over it. This erases the line by 
+			// setting the color of its pixels to white. 
+
+			if (fPrevLine)
+			{
+				MoveToEx(hdc, ptsBegin.x, ptsBegin.y,
+					(LPPOINT)NULL);
+				LineTo(hdc, ptsPrevEnd.x, ptsPrevEnd.y);
+			}
+
+			// Convert the current cursor coordinates to a 
+			// POINTS structure, and then draw a new line. 
+
+			ptsEnd = MAKEPOINTS(lParam);
+			MoveToEx(hdc, ptsBegin.x, ptsBegin.y, (LPPOINT)NULL);
+			LineTo(hdc, ptsEnd.x, ptsEnd.y);
+
+			// Set the previous line flag, save the ending 
+			// point of the new line, and then release the DC. 
+
+			fPrevLine = TRUE;
+			ptsPrevEnd = ptsEnd;
+			ReleaseDC(hWnd, hdc);
+		}
+		break;
+
+
+	case WM_LBUTTONUP:
+
+		// The user has finished drawing the line. Reset the 
+		// previous line flag, release the mouse cursor, and 
+		// release the mouse capture. 
+
+		fPrevLine = FALSE;
+		ClipCursor(NULL);
+		ReleaseCapture();
+		return 0;
+
+
 	//case WM_LBUTTONDOWN:
 	//{
 		//int iPosX = LOWORD(lParam);
@@ -149,21 +270,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		//wsprintf(waCoord,_T("(%i, %i)"),iPosX,iPosY);
 		//::MessageBox(hWnd,waCoord, _T("LBM Click"),MB_OK);
 	//}
-	case WM_LBUTTONDOWN: 
-		status = true;
-		break;
+	//case WM_LBUTTONDOWN: 
+		//status = true;
+		//break;
 
-	case WM_LBUTTONUP: 
-		status = false;
-		t.x = -1; pos.push_back(t);
-		break;
+	//case WM_LBUTTONUP: 
+		//status = false;
+		//t.x = -1; pos.push_back(t);
+		//break;
 
-	case WM_MOUSEMOVE: 
-		GetCursorPos(&t);
-		ScreenToClient(hWnd, &t);
-		(status) ? pos.push_back(t) : NULL;
-		(status) ? InvalidateRect(hWnd, NULL, true) : NULL;
-		break;
+	//case WM_MOUSEMOVE: 
+		//GetCursorPos(&t);
+		//ScreenToClient(hWnd, &t);
+		//(status) ? pos.push_back(t) : NULL;
+		//(status) ? InvalidateRect(hWnd, NULL, true) : NULL;
+		//break;
 
 	case WM_COMMAND:
 		wmId    = LOWORD(wParam);
@@ -193,9 +314,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		// TODO: добавьте любой код отрисовки...
 		hdc = BeginPaint(hWnd, &ps);
 	
-		(!pos.size()) ? NULL : MoveToEx(hdc, pos[0].x, pos[0].y, NULL);
-		for (size_t i = 0; i<pos.size(); ++i)
-			(pos[i].x == -1 && i<pos.size() - 1) ? MoveToEx(hdc, pos[i + 1].x, pos[i + 1].y, NULL) : LineTo(hdc, pos[i].x, pos[i].y);
+		//(!pos.size()) ? NULL : MoveToEx(hdc, pos[0].x, pos[0].y, NULL);
+		//for (size_t i = 0; i<pos.size(); ++i)
+		//	(pos[i].x == -1 && i<pos.size() - 1) ? MoveToEx(hdc, pos[i + 1].x, pos[i + 1].y, NULL) : LineTo(hdc, pos[i].x, pos[i].y);
 
 		
 		
